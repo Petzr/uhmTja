c599e060602e338aa96699cc23c4bb8688c38254
Peter <83911352+Petzr@users.noreply.github.com>
2022-12-07 14:04:04 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from './home.js';
+import TicketPage from './ticketPage.js';
+import Header from './header.js';
+
+function App() {
+  return <>
+    <Header />
+    <TicketPage />;
+  </>
+}
+
+export default App;
